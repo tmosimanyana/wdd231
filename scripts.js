cb294391e395309859bbfa21f2c930d2a552dd6d
Your Name <you@example.com>
2024-07-12 16:40:53 +0200

    files updated

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-// Your JavaScript code goes here
+//  JavaScript code goes here
 function filterCourses(category) {
     // Implement your course filtering logic here
 }
 
-// Example of setting current year dynamically
+//  set current year dynamically
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('currentyear').textContent = new Date().getFullYear();
 });
