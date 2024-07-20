018d69caafb3e8169060cd99364d778b1cc3cefb
Your Name <you@example.com>
2024-07-20 16:33:25 +0200

    discarged changes

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+//  JavaScript code goes here
+function filterCourses(category) {
+    // Implement your course filtering logic here
+}
+
+//  set current year dynamically
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('currentyear').textContent = new Date().getFullYear();
+});
