93f8cccd678bedb32deb78f6cbc50822b60d757e
Your Name <you@example.com>
2024-07-20 12:40:00 +0200

    files updated

--- chamber/js/form.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const timestampField = document.getElementById('timestamp');
-    const now = new Date();
-    timestampField.value = now.toISOString();
-});
