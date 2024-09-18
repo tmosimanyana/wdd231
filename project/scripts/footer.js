19fa6d0c8fe2050497664e23835a092b7eec353e
tmosimanyana <mbothepha@gmail.com>
2024-09-18 13:08:53 +0200

    deleted files

--- project/scripts/footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Set the current year
-    const currentYear = new Date().getFullYear();
-    document.getElementById('current-year').textContent = currentYear;
-
-    // Set the last modified date
-    const lastModified = document.lastModified;
-    document.getElementById('last-modified').textContent = lastModified;
-});
--- scripts.js
+++
@@ -1,9 +0,0 @@
-//  JavaScript code goes here
-function filterCourses(category) {
-    // Implement your course filtering logic here
-}
-
-//  set current year dynamically
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('currentyear').textContent = new Date().getFullYear();
-});
