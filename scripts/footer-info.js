4e5a887cdcd75712a07c57b145aac59087b55964
tmosimanyana <mbothepha@gmail.com>
2024-09-18 18:32:09 +0200

    FILES DELED

--- scripts/footer-info.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Update copyright year
-    const currentYearElem = document.getElementById('currentyear');
-    currentYearElem.textContent = new Date().getFullYear();
-
-    // Update last modified date
-    const lastModifiedElem = document.getElementById('lastModified');
-    lastModifiedElem.textContent = `Last modified: ${document.lastModified}`;
-});
--- scripts/navigation.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const nav = document.querySelector('nav');
-
-    menuToggle.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
-});
