54dd9fafccc60c8a865c68c546bada0b2f224375
tmosimanyana <mbothepha@gmail.com>
2024-09-26 17:50:23 +0200

    staged changes

--- scripts/dates.js
+++
@@ -1,7 +0,0 @@
-// Get the current year dynamically
-const currentYear = new Date().getFullYear();
-document.querySelector('footer p').textContent = `© ${currentYear} Rubia Magdalena Francesco | Madagascar`;
-
-// Display the last modified date of the document dynamically
-const lastModifiedDate = document.lastModified;
-document.querySelector('footer p:last-child').textContent = `Last Update: ${lastModifiedDate}`;
