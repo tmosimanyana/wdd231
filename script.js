70365bc9f1819bd93b498fff8afd7251ecb085d9
tmosimanyana <mbothepha@gmail.com>
2024-09-27 21:48:42 +0200

    files updated

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Course Data
 const courses = [
     { subject: 'CSE', number: 110, title: 'Introduction to Programming', credits: 2, completed: true },
-    { subject: 'WDD', number: 130, title: 'Web Fundamentals', credits: 2, completed: false },
+    { subject: 'WDD', number: 130, title: 'Web Fundamentals', credits: 2, completed: true },
     { subject: 'CSE', number: 111, title: 'Programming with Functions', credits: 2, completed: true },
     { subject: 'CSE', number: 210, title: 'Programming with Classes', credits: 2, completed: true },
-    { subject: 'WDD', number: 131, title: 'Dynamic Web Fundamentals', credits: 2, completed: false },
+    { subject: 'WDD', number: 131, title: 'Dynamic Web Fundamentals', credits: 2, completed: true },
     { subject: 'WDD', number: 231, title: 'Frontend Web Development I', credits: 2, completed: false }
 ];
 
