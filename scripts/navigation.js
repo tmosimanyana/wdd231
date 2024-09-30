f14d554954550d5ebe3dfb8c6b226fb56ec93efe
tmosimanyana <mbothepha@gmail.com>
2024-09-30 22:32:44 +0200

    deleted all files

--- scripts/navigation.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navList = document.querySelector('.nav-list');
-
-  menuToggle.addEventListener('click', () => {
-      navList.classList.toggle('active');
-  });
-});
