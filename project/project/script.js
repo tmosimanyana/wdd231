8e69cba0559a8e22e862ca98a33737af64b14a9b
tmosimanyana <mos23018@byui.edu>
2024-10-23 17:58:37 +0200

    updated file paths

---
+++ project/project/script.js
@@ -0,0 +1,4 @@
+// Main JavaScript file for Kweneng Agriculture Chamber of Commerce
+
+// Code can be added here for interactivity or other functionality
+console.log("Kweneng Agriculture Chamber of Commerce script loaded.");
