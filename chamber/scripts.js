77cdd435a8b0305f35c84f19e4aad52380e660e5
tmosimanyana <mos23018@byui.edu>
2024-10-28 14:16:31 +0200

    files updated

---
+++ chamber/scripts.js
@@ -0,0 +1,7 @@
+// Toggle menu script
+document.getElementById("menu-toggle").addEventListener("click", function () {
+    const nav = document.getElementById("main-nav");
+    nav.classList.toggle("active");
+  });
+  
+  
